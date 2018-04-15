37c13da1605e52a4d229cb2cb3d326e8aba7b509
jakemakesgames <jakebrown.designer@gmail.com>
2018-04-15 23:00:04 +1000

    changed creatures to require 0 mana for testing. cards can now be placed on field (base functionality)

--- Assets/My Assets/Scripts/Dragging/DragCreatureAttack.cs
+++ Assets/My Assets/Scripts/Dragging/DragCreatureAttack.cs
@@ -34,11 +34,12 @@ public class DragCreatureAttack : DraggingActions {
     public override bool CanDrag
     {
         get
-        {   
+        {
             // we can drag this card if 
             // a) we can control this our player (this is checked in base.canDrag)
             // b) creature "CanAttackNow" - this info comes from logic part of our code into each creature`s manager script
-            return base.CanDrag && manager.CanAttackNow;
+            return true;
+            // UNCOMMENT LATER - return base.CanDrag && manager.CanAttackNow;
         }
     }
 
