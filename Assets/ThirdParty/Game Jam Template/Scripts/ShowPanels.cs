645e1e42871dddcfb116a7c06870426a64cc02c7
jakemakesgames <jakebrown.designer@gmail.com>
2018-05-03 02:35:48 +1000

    added the global game jam menu template into the menu scene temporarily. Set up the options menu scripts again to all UI elements.

---
+++ Assets/ThirdParty/Game Jam Template/Scripts/ShowPanels.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPanels : MonoBehaviour {
+
+	public GameObject settingsPanel;						//Store a reference to the Game Object SettingsPanel 
+	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
+    public GameObject controlsPanel;                        //Store a reference to the Game Object ControlsPanel
+    public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+	public GameObject pausePanel;							//Store a reference to the Game Object PausePanel 
+
+    public GameObject creditsPanel;                         //Store a reference to the Game Object CrediitsPanel 
+
+    private bool isActive = true;
+
+    float fadeTime = 3f;
+    Color colorToFadeTo;
+
+    #region MenuPanel
+    //Call this function to activate and display the main menu panel during the main menu
+    public void ShowMenu()
+    {
+        menuPanel.SetActive(true);
+        isActive = true;
+    }
+
+    //Call this function to deactivate and hide the main menu panel during the main menu
+    public void HideMenu()
+    {
+        menuPanel.SetActive(false);
+        isActive = false;
+    }
+    #endregion
+
+    #region ControlsPanel
+    //Call this function to activate and display the Options panel during the main menu
+    public void ShowControlsPanel()
+    {
+        controlsPanel.SetActive(true);
+        isActive = false;
+    }
+
+    //Call this function to deactivate and hide the Options panel during the main menu
+    public void HideControlsPanel()
+    {
+        controlsPanel.SetActive(false);
+        isActive = true;
+    }
+    #endregion
+
+    #region SettingsPanel
+    //Call this function to activate and display the Options panel during the main menu
+    public void ShowSettingsPanel()
+	{
+        settingsPanel.SetActive(true);
+		optionsTint.SetActive(true);
+	}
+
+	//Call this function to deactivate and hide the Options panel during the main menu
+	public void HideSettingsPanel()
+	{
+        settingsPanel.SetActive(false);
+		optionsTint.SetActive(false);
+	}
+    #endregion
+
+    #region PausePanel
+    //Call this function to activate and display the Pause panel during game play
+    public void ShowPausePanel()
+	{
+		pausePanel.SetActive (true);
+		optionsTint.SetActive(true);
+	}
+
+	//Call this function to deactivate and hide the Pause panel during game play
+	public void HidePausePanel()
+	{
+		pausePanel.SetActive (false);
+		optionsTint.SetActive(false);
+
+	}
+    #endregion
+
+    #region CreditsPanel
+    //Call this function to activate and display the Credits panel during the main menu
+    public void ShowCreditsPanel()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    //Call this function to deactivate and hide the Credits panel during the main menu
+    public void HideCreditsPanel()
+    {
+        creditsPanel.SetActive(false);
+    }
+    #endregion
+}
