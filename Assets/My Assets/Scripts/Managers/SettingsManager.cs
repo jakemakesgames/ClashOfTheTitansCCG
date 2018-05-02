cad9ac4d45da533d45dae4c24702ef0c71d29297
jakemakesgames <jakebrown.designer@gmail.com>
2018-05-02 22:34:15 +1000

    basic options/ settings menu script working and implemented (can adjust master vol, toggle fullscreen, change between quality settings, change screen/ window resolution size in-game).

---
+++ Assets/My Assets/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour 
+{
+	public AudioMixer audioMixer;
+
+	[Header("Resolution Variables")]
+	public Dropdown resolutionDropdown;
+	Resolution[] resolutions;
+
+	void Start()
+	{
+		resolutions = Screen.resolutions;
+
+		resolutionDropdown.ClearOptions ();
+
+		List<string> options = new List<string>();
+
+		int currentResolutionIndex = 0;
+		for (int i = 0; i < resolutions.Length; i++) 
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			options.Add (option);
+
+			if (resolutions[i].width == Screen.currentResolution.width &&
+				resolutions[i].height == Screen.currentResolution.height) 
+			{
+				currentResolutionIndex = i;
+			}
+		}
+
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+	}
+
+
+	// Function to Set the Master Volume // 
+	public void SetVolume (float volume)
+	{
+		Debug.Log("Volume: " + volume);
+
+		audioMixer.SetFloat ("volume", volume);
+	}
+
+	// Function to Set the Quality Settings //
+	public void SetQuality (int qualityIndex)
+	{
+		QualitySettings.SetQualityLevel (qualityIndex);
+	}
+
+	// Function to Toggle Fullscreen //
+	public void SetFullscreen (bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
+	}
+
+	// Function to Set the Resolution //
+	public void SetResolution(int resolutionIndex)
+	{
+		Resolution resolution = resolutions [resolutionIndex];
+		Screen.SetResolution (resolution.width, resolution.height, Screen.fullScreen);
+	}
+
+}
