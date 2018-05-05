4f4a9a1b38815b8ed682841c8f6b4b0c4df6612a
jakemakesgames <jakebrown.designer@gmail.com>
2018-05-05 23:41:23 +1000

    added quick and rough pause menu

---
+++ Assets/My Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour 
+{
+
+	public string menuName;
+
+	public GameObject pauseCanvas;
+	public bool isPaused;
+
+	void Awake()
+	{
+		pauseCanvas.SetActive (false);
+	}
+
+	public void Start()
+	{
+		isPaused = false;
+	}
+
+	public void Update()
+	{
+		if (Input.GetButtonDown ("Cancel")) 
+		{
+			DoPause ();
+		}
+		else if (Input.GetButtonDown ("Cancel") && isPaused == true) 
+		{
+			UnPause ();
+		}
+	}
+
+	public void DoPause()
+	{
+		isPaused = true;
+		Time.timeScale = 0;
+		pauseCanvas.SetActive (true);
+	}
+
+	public void UnPause()
+	{
+		isPaused = false;
+		Time.timeScale = 1;
+		pauseCanvas.SetActive (false);
+	}
+
+	public void ReturnToMenu()
+	{
+		SceneManager.LoadScene (menuName);
+	}
+}
